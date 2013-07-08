cf8b6a4a26c34282283ce567cc098f08f5443518
Ubuntu <ubuntu@ip-172-31-43-172.us-west-2.compute.internal>
2013-07-08 14:14:20 +0000

    update url grade.js

--- grader.js
+++ grader.js
@@ -17,6 +17,15 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url) {
+    var instr = url.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -39,6 +48,16 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+var checkHtmlUrl = function(url, checksfile) {
+    $ =resturl(rul);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -50,11 +69,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-l, --url <url>', 'Path of url', clone(assertFileExists),CHECKSRUL_DEFAULT)
+        .option('-l, --url <url>', 'Path of url', clone(asserUrlExists),CHECKSRUL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson2=checkUrlFIle(program.url);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+}
+
+   var outJson2 = JSON.stringify(checkJson2, null, 4);
+    console.log(outJson2);
+} else {
+    exports.checkUrlFile = checkUrlFile;
 }
